aa9c529cf8b83cda5eec85efa6f6d756f701a3d9
xwasd00 <xsovam00@stud.fit.vutbr.cz>
2020-04-30 17:37:13 +0200

    tisknuti portu

--- ipk-sniffer.cpp
+++ ipk-sniffer.cpp
@@ -3,15 +3,51 @@
 #include "Args.h"
 #include <netinet/ip.h>
 #include <netinet/ip6.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 #include <bitset>
 
 using namespace std;
 
 #define ETHERNET_SIZE 14
+#define IPv4_MIN_SIZE 20
+#define TCP_PROTOCOL 6
+
+void tcpGetInfo(tcphdr* tcp, u_short* src_port, u_short* dst_port, u_char* payload){
+		*src_port = tcp->th_sport<<8 | tcp->th_dport>>8;
+		*dst_port = tcp->th_dport<<8 | tcp->th_dport>>8;
+		payload = (u_char*)(tcp + tcp->th_off * 4);
+		cout << endl << tcp->th_ack << endl;
+		return;
+}
+
+void udpGetInfo(udphdr* udp, u_short* src_port, u_short* dst_port, u_char* payload){
+		*src_port = udp->uh_sport<<8 | udp->uh_sport>>8;
+		*dst_port = udp->uh_dport<<8 | udp->uh_dport>>8;
+		payload = (u_char*)(udp + sizeof(udphdr));
+		return;
+}
 
 void ipv4Header(ip* ip_header){
-	cout << "ipv4" << endl;
+	if ( ip_header->ip_hl * 4 < IPv4_MIN_SIZE){
+		cerr << "ipv4 wrong header" <<endl;
+		return;
+	}
+	u_short src_port, dst_port;
+	u_char* payload;
+	if(ip_header->ip_p == TCP_PROTOCOL){
+		tcphdr* tcp = (tcphdr*)((const u_char*)ip_header + ip_header->ip_hl * 4);
+		tcpGetInfo( tcp, &src_port, &dst_port, payload );
+	}
+	else{
+		udphdr* udp = (udphdr*)((const u_char*)ip_header + ip_header->ip_hl * 4);
+		udpGetInfo( udp, &src_port, &dst_port, payload );
+	}
+	//cout << "src_a: " << ip_header->ip_src.s_addr << " , dst_a: " << ip_header->ip_dst.s_addr << endl;
+	cout << "src_p: " << src_port << " , dst_p: " << dst_port << endl;
+
+
 }
 
 void ipv6Header(ip6_hdr* ip_header){
@@ -19,7 +55,9 @@ void ipv6Header(ip6_hdr* ip_header){
 }
 
 
-void callback(u_char *user, const struct pcap_pkthdr *header, const u_char *packet){
+void callback(u_char* user, const struct pcap_pkthdr* header, const u_char* packet){
+	cout << header->ts.tv_sec;
+
 	ip* ip_header = (ip*)(packet + ETHERNET_SIZE);
 	if (ip_header->ip_v != 4){
 		ip6_hdr* ip6_header = (ip6_hdr*)(ip_header);
